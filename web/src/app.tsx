a2ec8e5ecd2eadf6ad033b0fc83d80b6d148e265
leonan <leonanorth3@gmail.com>
2025-05-27 19:50:29 -0300

    feat: desktop layout

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -27,24 +27,24 @@ export function App() {
   }, [])
 
   return (
-    <div className="flex min-h-dvh flex-col justify-center bg-gray-200">
-      <div className="max-w-[980px] space-y-8 px-3 py-8">
+    <div className="flex min-h-dvh flex-col justify-center bg-gray-200 lg:items-center">
+      <div className="w-full max-w-[996px] space-y-8 px-3 py-8">
         <img
           src="/logo.svg"
           alt="brev.ly"
           width={96.67}
           height={24}
-          className="mx-auto"
+          className="mx-auto md:mx-0"
         />
 
-        <div className="flex w-full flex-col gap-3">
-          <div className="space-y-5 rounded-lg bg-white p-6">
+        <div className="flex w-full flex-col gap-3 md:flex-row">
+          <div className="w-full space-y-5 rounded-lg bg-white p-6 md:max-w-[380px]">
             <h2 className="text-lg font-bold text-gray-600">Novo link</h2>
 
             <CreateShortLinkForm />
           </div>
 
-          <div className="space-y-4 rounded-lg bg-white p-6">
+          <div className="w-full space-y-4 rounded-lg bg-white p-6">
             <div className="flex items-center justify-between">
               <h2 className="text-lg font-bold text-gray-600">Meus links</h2>
 
