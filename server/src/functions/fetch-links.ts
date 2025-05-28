d20d172b707bb1c1f7da9612de5bc0215263fe8d
leonan <leonanorth3@gmail.com>
2025-05-28 00:22:35 -0300

    feat: sort links by access count

--- server/src/functions/fetch-links.ts
+++ server/src/functions/fetch-links.ts
@@ -1,6 +1,8 @@
+import { desc } from 'drizzle-orm'
+
 import { db } from '@/infra/db'
 import { schema } from '@/infra/db/schema'
 
 export function fetchLinks() {
-  return db.select().from(schema.links)
+  return db.select().from(schema.links).orderBy(desc(schema.links.accessCount))
 }
